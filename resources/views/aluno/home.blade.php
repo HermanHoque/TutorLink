d2f792bc2f9a49a3d15c18e179209108e167fef6
HermanHoque <hermanhoque13@gmail.com>
2025-02-21 23:58:32 +0100

    alterações no home_aluno

--- resources/views/aluno/home.blade.php
+++ resources/views/aluno/home.blade.php
@@ -1,5 +1,83 @@
 @extends('aluno/layout')
 
+
 @section('layout_user')
-    <h1>home</h1>
+
+    <div class="row">
+        <div class="col" style="font-size: 30pt">
+            <i class="bi bi-house"></i>
+            <strong>Home</strong>
+        </div>
+    </div>
+
+    {{-- formulario de pesquisa --}}
+    <form action="" method="get">
+        <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="buscaid" style="background: #3C4049">
+                <i class="bi bi-search"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Pesquisar..." aria-label="Username" aria-describedby="addon-wrapping">
+
+            <div class="dropdown" >
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"
+                style="background: #3C4049; margin-left: 10px;">
+                    <i class="bi bi-filter"></i> Filtrar
+                </button>
+
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Pesquisar por nome</a></li>
+                    <li><a class="dropdown-item" href="#">Pesquisar por especialidade</a></li>
+                </ul>
+            </div>
+        </div>
+    </form>
+
+    <div style="margin-top:10px; margin-bottom: 15px;">
+        <a class="btn btn-outline-secondary" href="">Todos</a>
+        <a class="btn btn-outline-secondary" href="">Melhores avaliações</a>
+    </div>
+
+
+    {{-- cards --}}
+    @for ($i = 0; $i <= 1; $i++)
+    <div class="container text-center">
+
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" id="btnH1" class="btn">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" id="btnH1" class="btn">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" id="btnH1" class="btn">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+    <br>
+    @endfor
+   
 @endsection
--- resources/views/aluno/layout.blade.php
+++ resources/views/aluno/layout.blade.php
@@ -7,7 +7,10 @@
     <link rel="shortcut icon" href="{{ asset('img/TutorLinkLogo.svg') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/Edit_bootstrap.css') }}">
     <style>
         body {
             margin: 0;
@@ -63,6 +66,11 @@
         .sidebar:hover a span {
             opacity: 1;
         }
+
+    #btnH1{
+        background: #3C4049;
+        color: white 
+    }
     </style>
 </head>
 <body>
@@ -80,7 +88,7 @@
         <a id="op" href="#"><i class="bi bi-box-arrow-right"></i><span> Sair</span></a>
     </div>
 
-    <div style="margin: 10px">
+    <div style="padding-left: 20px; padding-top: 20px; width: 80%">
         @yield('layout_user')
     </div>
 
